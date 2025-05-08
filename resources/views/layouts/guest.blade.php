e042603c2b57e5587df589d4df59d8633c94e2f7
FortuneJames <oriebuluechendu123@gmail.com>
2025-05-08 17:41:00 +0100

    livechat

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -23,6 +23,27 @@
     <link rel="stylesheet" href="assets/css/rs6.css" />
     <link rel="stylesheet" href="assets/css/slick-theme.css" />
     <link rel="stylesheet" href="assets/css/style.css" />
+
+
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+        _smartsupp.key = '74a07d8b2890aa02a94148668c71e0e7043a1a39';
+        window.smartsupp || (function(d) {
+            var s, c, o = smartsupp = function() {
+                o._.push(arguments)
+            };
+            o._ = [];
+            s = d.getElementsByTagName('script')[0];
+            c = d.createElement('script');
+            c.type = 'text/javascript';
+            c.charset = 'utf-8';
+            c.async = true;
+            c.src = 'https://www.smartsuppchat.com/loader.js?';
+            s.parentNode.insertBefore(c, s);
+        })(document);
+    </script>
+    <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 </head>
 
 <body>
@@ -234,6 +255,8 @@
     <script src="assets/js/knob.js"></script>
     <script src="assets/js/typer.js"></script>
     <script src="assets/js/script.js"></script>
+
+
 </body>
 
 
